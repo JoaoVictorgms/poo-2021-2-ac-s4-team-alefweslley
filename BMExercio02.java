f46ef5fdb1c93c01fea72445d872c9e47e8a89dd
alefifpb <87621475+alefifpb@users.noreply.github.com>
2021-11-24 15:18:28 -0300

    Create BMExercio02.java
    add questão 2 BM

---
+++ BMExercio02.java
@@ -0,0 +1,29 @@
+
+/**
+ * Escreva uma descrição da classe BMExercio02 aqui.
+ * 
+ * @author (alef) 
+ * @version (um número da versão ou uma data)
+ */
+public class BMExercio02
+{ 
+    public static void main(String[] args) {
+    
+    int i = 1;
+    long j = 2;
+    float p= 20.0f;
+    double q = 30.0;
+    boolean b = true;
+    char c = 'k';
+    
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(b);
+        System.out.println (c);
+ 
+    
+    }
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+}
