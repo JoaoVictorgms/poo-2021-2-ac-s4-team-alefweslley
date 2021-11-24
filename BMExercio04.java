a5b2da796f0e6dd5babb0d9963120e425ef3ccb6
alefifpb <87621475+alefifpb@users.noreply.github.com>
2021-11-24 17:32:22 -0300

    Questão 4 Batista & Moraes

---
+++ BMExercio04.java
@@ -0,0 +1,21 @@
+/**
+ * Escreva uma descrição da classe BMExercio02 aqui.
+ * 
+ * @author (alef) 
+ * @version (um número da versão ou uma data)
+ */
+public class BMExercio03
+{ 
+    public static void main(String[] args) {
+    
+    
+     long long1 = 100;
+     int int1= 200;
+     int long2=Math.toIntExact(long1);
+     int int2= int1+long2;
+     
+     System.out.println(int2);
+    
+    }
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+}
