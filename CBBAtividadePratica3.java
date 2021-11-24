e17ddb1e67b31125e33b56df15373db0c6a665e3
Francisco Weslley Silva de Souza <44758448+weslleycz@users.noreply.github.com>
2021-11-23 21:42:58 -0300

    Chagas, Baruque & Baruque (2010) 3 (pg. 60)

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Weslley
+ */
+public class CBBAtividadePratica3 {
+    public static void main(String[] args){
+    float base=10;
+    float altura=5;
+    System.out.println("Base:");
+    System.out.println(base);
+    System.out.println("Altura:");
+    System.out.println(altura);
+    System.out.println("Área:");
+    System.out.println(CalculaArea(base,altura));
+    }
+    public static float CalculaArea(float n1,float n2){
+     return n1*n2;
+    }
+ }
